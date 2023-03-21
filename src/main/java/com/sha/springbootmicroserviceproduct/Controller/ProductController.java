6bfa536851b7774b9e7c1e97ec2d4ec0d2fd687b
dogan <dogan.bacaksiz@gmail.com>
2023-03-21 23:54:50 +0300

    controller sınıfı eklendi

--- src/main/java/com/sha/springbootmicroserviceproduct/Controller/ProductController.java
+++ src/main/java/com/sha/springbootmicroserviceproduct/Controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.sha.springbootmicroserviceproduct.Controller;
 
+import com.sha.springbootmicroserviceproduct.Model.ProductDto;
 import com.sha.springbootmicroserviceproduct.Service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/product") //pre path
@@ -11,6 +15,24 @@ public class ProductController {
     @Autowired
     private IProductService productService;
 
-    public Respı
+    @PostMapping
+    public ResponseEntity<?> saveProduct(@RequestBody ProductDto productDto) {
+        return new ResponseEntity<>(productService.saveProduct(productDto), HttpStatus.CREATED);
+    }
 
+    @PutMapping
+    public ResponseEntity<?> updateProduct(@RequestBody ProductDto productDto) {
+        return new ResponseEntity<>(productService.updateProduct(productDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{productUuid}")
+    public ResponseEntity<?> deleteProduct(@PathVariable UUID productUuid) {
+        productService.deleteProduct(productUuid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    ResponseEntity<?> getAllProducts() {
+        return new ResponseEntity<>(productService.findAllProducts(), HttpStatus.OK);
+    }
 }
