8da1ea52a1f7483760c69f02f7c6bc09fc9567eb
dogan <dogan.bacaksiz@gmail.com>
2023-03-21 22:28:24 +0300

    initial commit

---
+++ src/main/java/com/sha/springbootmicroserviceproduct/SpringBootMicroserviceProductApplication.java
@@ -0,0 +1,14 @@
+package com.sha.springbootmicroserviceproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroserviceProductApplication.class, args);
+    }
+
+
+}
