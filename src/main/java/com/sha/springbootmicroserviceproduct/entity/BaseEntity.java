2b1bc518762233538d59ef213feeeb25b3605cb1
dogan <dogan.bacaksiz@gmail.com>
2023-03-21 23:12:31 +0300

    Base ve Product entity

---
+++ src/main/java/com/sha/springbootmicroserviceproduct/entity/BaseEntity.java
@@ -0,0 +1,53 @@
+package com.sha.springbootmicroserviceproduct.entity;
+
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "uuid", updatable = false, nullable = false)
+    private UUID uuid;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    private Integer version;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        BaseEntity that = (BaseEntity) o;
+        return getUuid() != null && Objects.equals(getUuid(), that.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/java/com/sha/springbootmicroserviceproduct/entity/Product.java
@@ -0,0 +1,36 @@
+package com.sha.springbootmicroserviceproduct.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product extends BaseEntity {
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Product product = (Product) o;
+        return getUuid() != null && Objects.equals(getUuid(), product.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
