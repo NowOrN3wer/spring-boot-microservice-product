49c567980734ef854e0fbeb18f9a6a3b554ac84f
doganbacaksiz <dogan.bacaksiz@gmail.com>
2023-03-22 10:54:37 +0300

    servisler

--- src/main/java/com/sha/springbootmicroserviceproduct/Security/SecurityConfig.java
+++ src/main/java/com/sha/springbootmicroserviceproduct/Security/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.sha.springbootmicroserviceproduct.Security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,9 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
-    @Value("$service.security.secure-key-username")
+    @Value("${service.security.secure-key-username}")
     private String SECURE_USER_NAME;
-    @Value("$service.security.secure-key-password")
+    @Value("${service.security.secure-key-password}")
     private String SECURE_USER_PASSWORD;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
